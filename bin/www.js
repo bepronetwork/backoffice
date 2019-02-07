2fc80091c7bdebe060c73560bd71d837cbac6568
Rui Pedro <ruipedro@Ruis-MacBook-Air.local>
2019-02-07 21:10:35 +0000

    EDIT - bin/www - Accept All Routes

--- bin/www.js
+++ bin/www.js
@@ -8,6 +8,8 @@ const PORT  = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
