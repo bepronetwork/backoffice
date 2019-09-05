53266dad0d1775de042f351c548147b80f814d55
ranierigalasso <ranierigalasso@gmail.com>
2019-09-05 16:35:51 +0100

    completed announcementTb customization

---
+++ src/containers/Applications/Customization/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Col, Container, Row, Card, CardBody, Button } from 'reactstrap';
+import AnnouncementTabSettings from './components/AnnouncementTabSettings';
+
+export default class CustmoizationContainer extends Component {
+    render() {
+        return (
+            <Container className="dashboard">
+            <p className="dashboard__visitors-chart-title text-left" style={{fontSize : 18, marginBottom : 10}}> Customization Settings </p>
+            <hr></hr>
+            <Row>
+                <Col lg={4}>
+                    <AnnouncementTabSettings />
+                </Col>
+            </Row>
+      </Container>
+        )
+    }
+}
+
