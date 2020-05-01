d07ecaab7643ebeb69042eeac9c6ff8c6abc8603
Wellington Matheus <w.matheus@outlook.com>
2020-05-01 20:00:46 -0300

    feat: create Logs subtab

---
+++ src/containers/Settings/tabs/LogsContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+import LogsTable from './Components/LogsTable';
+
+
+export default class LogsContainer extends React.Component{
+
+    render = () => {
+
+        return (
+            <div>               
+                <p className="dashboard__visitors-chart-title text-left text-red" style={{fontSize : 18, marginBottom : 10}}> Logs </p>
+                <hr/>
+                <Paper style={{ boxShadow: "0 10px 30px 1px rgba(0, 0, 0, 0.06)", padding: 25 }} >
+                    <LogsTable/>
+                </Paper>
+          </div>
+        )
+    }
+
+};
