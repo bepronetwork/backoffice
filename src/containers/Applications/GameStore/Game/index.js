911a48a7f871b78653cbda0733b73e9a7ede0522
Wellington Matheus <w.matheus@outlook.com>
2020-06-05 06:38:54 -0300

    feat: change game image in Game Store

--- src/containers/Applications/GameStore/Game/index.js
+++ src/containers/Applications/GameStore/Game/index.js
@@ -2,6 +2,7 @@
 import React, { PureComponent } from 'react';
 import { Card, CardBody, Col, Row, Button } from 'reactstrap';
 import { AddIcon } from 'mdi-react';
+import game_images from '../../components/game_images';
 
 class GameStoreContainer extends PureComponent {
  
@@ -24,13 +25,17 @@ class GameStoreContainer extends PureComponent {
         const { isLoading } = this.state;
         if(!game){return null}
         const { name, description, image_url } = game;
+
+        const game_image = game_images[String(name).toLowerCase().replace(/ /g,"_")];
+        const image = game_image ? game_image : game_images.default;
+
         return (
             <Col md={12} xl={12} lg={12} xs={12} style={{ minWidth: 288, maxWidth: 415, height: 230 }}>
                 <Card className='game-container'>
                     <CardBody className="dashboard__card-widget" style={{ borderRadius: "10px", border: "solid 1px rgba(164, 161, 161, 0.35)", backgroundColor: "#fafcff", boxShadow: "none" }}>
                         <Row>
                             <Col lg={4} >  
-                                <img className='application__game__image' style={{display: 'block', marginLeft: `-10px`}} src={image_url}/>
+                                <img className='application__game__image' style={{display: 'block', height: 50, width: 50 }} src={image} alt={name}/>
                             </Col>
                             <Col lg={8} >
                                 <div className="dashboard__visitors-chart text-left">
