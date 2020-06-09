88a4b59cd59dc8a73b09530ee0377a7ed3483fae
Wellington Matheus <w.matheus@outlook.com>
2020-06-09 06:44:44 -0300

    feat: add new initial page

--- src/containers/Wizards/CreateApp/components/WizardFormOne.jsx
+++ src/containers/Wizards/CreateApp/components/WizardFormOne.jsx
@@ -1,13 +1,33 @@
 import React, { PureComponent } from 'react';
-import { Row, Col, CardBody, Card } from 'reactstrap';
+import { Row, Col, CardBody, Card, FormGroup } from 'reactstrap';
 import { reduxForm } from 'redux-form';
 import { connect } from "react-redux";
 import { compose } from 'lodash/fp'
 import { ApplicationIcon, DesktopMacDashboardIcon, CoinIcon, EthereumIcon } from 'mdi-react';
 import TextInput from '../../../../shared/components/TextInput';
+import TextLoop from 'react-text-loop';
+import { BackgroundBox, VerticalSection, BetProtocolLogo, Container, CreateAppCard, CardHeader, CardContent, InputLabel, NameInput, DescriptionInput, Footer } from '../styles';
 const Back = `${process.env.PUBLIC_URL}/img/dashboard/background-login.png`;
 const loading = `${process.env.PUBLIC_URL}/img/loading.gif`;
 
+const words = [
+    { id: 0, text: 'BetProtocol' },
+    { id: 1, text: 'Scalable' },
+    { id: 2, text: 'Secure & Audited' },
+    { id: 3, text: 'No Coding Required' },
+  ];
+  
+  const Description = (props) => {
+    const { wordList } = props;
+  
+    return (
+      <TextLoop>
+        {wordList.map((word) => (
+          <span key={word.id}>{word.text}</span>
+        ))}
+      </TextLoop>
+    );
+  };
 
 class WizardFormOne extends PureComponent {
 
@@ -35,7 +55,31 @@ class WizardFormOne extends PureComponent {
 
         this.setState({ virtual : !virtual });
     };
-    
+
+    onChangeName = value => {
+        if (value) {
+            this.setState({
+                name: value
+            })
+        } else {
+            this.setState({
+                name: null
+            })
+        }
+    }
+
+    onChangeDescription = value => {
+        if (value) {
+            this.setState({
+                description: value
+            })
+        } else {
+            this.setState({
+                description: null
+            })
+        }
+    }
+
     createApp = async () => {
         try{
             this.setState({isLoading : true});
@@ -57,38 +101,42 @@ class WizardFormOne extends PureComponent {
 
 	render() {
         const { virtual } = this.state;
-		const { handleSubmit } = this.props;
+        const { handleSubmit } = this.props;
 
 		return (
-            <div className={'container__all'}>
-				<Row className={'container__all'}  style={{marginTop : '10%'}}>
-                    <Col lg={6} className={'login_background indexed'} >
-						<img className="login_background indexed" src={Back} />
-					</Col>
-                    <Col lg={6}>
-                    <div className="account__wrapper">
-							<div className="account__card">
-							
-                            <form className="form" onSubmit={handleSubmit}>
-                        <div className="form__form-group">
-                            <h3 className="wizard__title">Create your First Application</h3>
-                                <TextInput
-                                    icon={ApplicationIcon}
-                                    name="name"
+            <>
+			<BackgroundBox>
+			   <VerticalSection>
+				   <ul>
+					   <BetProtocolLogo />
+					   <Description wordList={words} />
+				   </ul>
+				   <Container>
+					   <CreateAppCard>
+					   <CardHeader />
+						   <CardContent>
+                            <h1>Create your First Application</h1>
+                                <FormGroup>
+                                    <InputLabel for="name">App</InputLabel>
+                                <NameInput
                                     label="App"
+                                    name="name"
                                     type="text"
-                                    placeholder="RiskYou"
-                                    changeContent={this.changeContent}
+                                    // defaultValue={this.state.username}
+                                    onChange={(e) => this.onChangeName(e.target.value)}
                                 />
-                                <TextInput
-                                    icon={DesktopMacDashboardIcon}
-                                    name="description"
+                                </FormGroup>
+                                <FormGroup>
+                                    <InputLabel for="description">Description</InputLabel>
+                                <DescriptionInput
                                     label="Description"
+                                    name="description"
                                     type="text"
-                                    placeholder="A Great Casino Platform for Risk Users"
-                                    changeContent={this.changeContent}
+                                    // defaultValue={this.state.username}
+                                    onChange={(e) => this.onChangeDescription(e.target.value)}
                                 />
-                                <div style={{width : "100%", display : "inline-flex", margin : "20px 0 0 28px"}}>
+                                </FormGroup>
+                                <div style={{width : "100%", display : "inline-flex", justifyContent: "center" }}>
                                     <div style={{ width : 130, textAlign : "center" }}>
                                         <Card style={{paddingBottom : 10, cursor : "pointer"}} onClick={() => this.selectMoneyType('real')}>
                                             <CardBody style={{padding : 10, backgroundColor: !virtual ? "rgba(137, 71, 152, 0.1)" : "#fff", border: !virtual ? "1px solid #894798" : "1px solid #bebdbd"}}>
@@ -114,21 +162,25 @@ class WizardFormOne extends PureComponent {
                                         </Card>
                                     </div>
                                 </div>
-                            </div>
                     
-                            <div className="account__btns">
+                            <div className="account__btns" style={{ justifyContent: "center" }}>
                                 <button disabled={this.state.isLoading} style={{maxWidth : 350, marginTop  :30}}  onClick={() => this.createApp()} c className="btn btn-primary account__btn">
                                     {this.state.isLoading ? <img src={loading} style={{width : 20}}/> : 'Register App'}
                                 </button>
                             </div>
-                          
-                        </form>
-                        </div>
-                        </div>
-		                    
-                    </Col>
-                </Row>
-            </div>
+
+                    </CardContent>
+                    </CreateAppCard>
+                        <Footer>
+                            <span>
+                                <b>@BetProtocol</b> Technology is a SaaS Platform that provides
+                                infrastructure for Gaming Applications
+                            </span>
+                        </Footer>
+                </Container>
+                </VerticalSection>
+            </BackgroundBox>
+        </>           
 		);
 	}
 }
