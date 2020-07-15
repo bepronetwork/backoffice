670d88a48ab86ae97f66b2c2fa12a18f34efe91a
Wellington Matheus <w.matheus@outlook.com>
2020-07-15 16:55:13 -0300

    feat: add fake data

---
+++ src/containers/Bets/components/EsportsBetsTable/data.js
@@ -0,0 +1,206 @@
+export default [
+    {
+        "_id": "5ea22736eaa4b90021b30759",
+        "result": [
+          {
+            "_id": "5ea22736eaa4b90021b30758"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Head",
+          "start": 50,
+          "end": 100,
+          "probability": 0.5,
+          "index": 1
+        },
+        "isJackpot": false,
+        "isWon": false,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:34.484Z",
+        "nonce": 829922,
+        "clientSeed": "8b9489a3c5dc7",
+        "serverHashedSeed": "e772da248fac43a495fac320f2146b0bd77fc459c8912b56f733598e3dfb318e4a3de726828343a803286a83f65816cb4d831522a952e7b03cd018f64785e552",
+        "serverSeed": "14f2fed0184a94",
+        "__v": 0
+      },
+      {
+        "_id": "5ea227360c874b0021c1625b",
+        "result": [
+          {
+            "_id": "5ea227360c874b0021c1625a"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Tails",
+          "start": 0,
+          "end": 50,
+          "probability": 0.5,
+          "index": 0
+        },
+        "isJackpot": false,
+        "isWon": true,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0.02,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:34.276Z",
+        "nonce": 2014733,
+        "clientSeed": "10c83ea112e947",
+        "serverHashedSeed": "0f67dc51098fdf2c0372b18f7fbf2f6a4643b5b2dbfd68b0a48bb7a2cabe87487b2d693366a41ab57729ede64c761ddb3f9700df77e77ce6f543cde1eaa79389",
+        "serverSeed": "184a9bda19db1",
+        "__v": 0
+      },
+      {
+        "_id": "5ea22736eaa4b90021b30757",
+        "result": [
+          {
+            "_id": "5ea22736eaa4b90021b30756"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Tails",
+          "start": 0,
+          "end": 50,
+          "probability": 0.5,
+          "index": 0
+        },
+        "isJackpot": false,
+        "isWon": true,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0.02,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:34.222Z",
+        "nonce": 1875081,
+        "clientSeed": "158d378dc31d87",
+        "serverHashedSeed": "619f14292f1abf7ef9e7c9e1413c3875b49c6648a2b0837a8c39ccc6a0a98b6ec38fc03ce7392654937296aad332109828ce030c472022a51abeb31f6fcb24df",
+        "serverSeed": "147ef4bf5a5881",
+        "__v": 0
+      },
+      {
+        "_id": "5ea22736a0d02b0021c1a2a3",
+        "result": [
+          {
+            "_id": "5ea22736a0d02b0021c1a2a2"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Tails",
+          "start": 0,
+          "end": 50,
+          "probability": 0.5,
+          "index": 0
+        },
+        "isJackpot": false,
+        "isWon": true,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0.02,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:34.101Z",
+        "nonce": 1226374,
+        "clientSeed": "16f39b48640b53",
+        "serverHashedSeed": "3f5b5b2dbfce703a880347ed814afe64ab9366b1c3535362bf552b99cd4ebdc21c920332ffee8e7907105b6042c8fdf812fee99bf074ad72ace1cf7bdeddab38",
+        "serverSeed": "7f4342b9c3e18",
+        "__v": 0
+      },
+      {
+        "_id": "5ea227350c874b0021c16259",
+        "result": [
+          {
+            "_id": "5ea227350c874b0021c16258"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Tails",
+          "start": 0,
+          "end": 50,
+          "probability": 0.5,
+          "index": 0
+        },
+        "isJackpot": false,
+        "isWon": true,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0.02,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:33.767Z",
+        "nonce": 839307,
+        "clientSeed": "3f5aff6dfdfcf",
+        "serverHashedSeed": "5c9d7b6a2ea8114a9a14c8c99a052d34f573b835a7a4eaffc420ce159e9c020e0b51b282d8bf2a93f56ab28916af6b15a39cc5cb6de15488c3dbd08b13f0ef0b",
+        "serverSeed": "124901a3355caf",
+        "__v": 0
+      },
+      {
+        "_id": "5ea22732eaa4b90021b30754",
+        "result": [
+          {
+            "_id": "5ea22731eaa4b90021b30753"
+          }
+        ],
+        "isResolved": true,
+        "currency": "5e108498049eba079930ae1c",
+        "user": {
+          "_id": "5e5bfd189517230021a8c99a",
+          "username": "matheusteste"
+        },
+        "app": "5e48d6a928c1af0021c366d6",
+        "outcomeResultSpace": {
+          "key": "Head",
+          "start": 50,
+          "end": 100,
+          "probability": 0.5,
+          "index": 1
+        },
+        "isJackpot": false,
+        "isWon": false,
+        "game": "5e72701e56c3f10021fc67ee",
+        "winAmount": 0,
+        "betAmount": 0.01,
+        "fee": 0,
+        "timestamp": "2020-04-23T23:39:29.595Z",
+        "nonce": 742818,
+        "clientSeed": "1e864608f6a153",
+        "serverHashedSeed": "890c128ecce521cd8924e97883210727a40a73403e6d0670a5ac8c20a1b284d757e5348b8d00e13575a7a607a44646a01a156f34412555e78a1018eaae36b21d",
+        "serverSeed": "1f9f201f1e6e89",
+        "__v": 0
+      }
+]
