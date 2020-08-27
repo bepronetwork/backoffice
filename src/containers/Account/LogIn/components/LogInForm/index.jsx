fbb5081c18b7260e0ed63ee93b95d2b1c5433b25
Wellington Matheus <w.matheus@outlook.com>
2020-08-27 10:25:54 -0300

    fix: move Login form component to own folder

--- src/containers/Account/LogIn/components/LogInForm/index.jsx
+++ src/containers/Account/LogIn/components/LogInForm/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Input, Button, Link } from '../../../../components';
+import { Input, Button, Link } from '../../../../../components';
 
 import { Form, Label, ForgotPassword, Actions } from './styles';
 
