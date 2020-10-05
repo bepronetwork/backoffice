1f4289900225f40f20d0acf6a4301ef1172ab5ad
Wellington Matheus <w.matheus@outlook.com>
2020-10-05 19:55:13 -0300

    feat: add websocket env

--- src/esports/WebSocket/index.js
+++ src/esports/WebSocket/index.js
@@ -1,8 +1,10 @@
 import io from 'socket.io-client';
 import { createContext } from 'react';
 
+export const WEBSOCKET_ESPORTS_URL = process.env.REACT_APP_WEBSOCKET_ESPORTS;
+
 const socketConnection = createContext({
-    connection: io("wss://esports-listener-dev.herokuapp.com", {
+    connection: io(WEBSOCKET_ESPORTS_URL, {
       transport: ["websocket"]
     })
 });
