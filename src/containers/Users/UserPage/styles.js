0856edeb34247bf81e2c1224cb2ca099f9f77e12
Wellington Matheus <w.matheus@outlook.com>
2020-10-07 12:22:25 -0300

    feat: disable user esports bets

--- src/containers/Users/UserPage/styles.js
+++ src/containers/Users/UserPage/styles.js
@@ -4,3 +4,22 @@ export const ConvertContainer = styled.div`
     padding: 25px;
     width: 300px;
 `;
+
+export const EsportsNotEnable = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    > img {
+        height: 70px;
+        width: 70px;
+
+        margin: 0px 10px;
+    }
+
+    > span {
+        font-size: 18px;
+        font-weight: 500;
+        color: #814c94;
+    }
+`;
